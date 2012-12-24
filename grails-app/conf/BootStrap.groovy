93022891b4927fc5fc045bcd83afba010c950887
personal <personal@personal-PC>
2012-12-25 00:51:55 +0530

    first uncomment code

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,18 +4,18 @@ import grails.util.GrailsUtil
 class BootStrap {
 
   def init = { servletContext ->
-    switch(GrailsUtil.environment){
-      case "development":
-        def admin = new User(login:"admin", password:"password", name:"Bhupender")
-        admin.save()
-        
-        def normal = new User(login:"user", password:"password", name:"Amit")
-        normal.save()              
-      break
-
-      case "production":
-      break
-    }
+//    switch(GrailsUtil.environment){
+//      case "development":
+//        def admin = new User(login:"admin", password:"password", name:"Bhupender")
+//        admin.save()
+//        
+//        def normal = new User(login:"user", password:"password", name:"Amit")
+//        normal.save()              
+//      break
+//
+//      case "production":
+//      break
+//    }
 
   }
   def destroy = {
